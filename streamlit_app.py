08c0b214298c39a68ed68b84f58c8ab3e2ed6e71
rasyarizqullah <rasyarizqulla@gmail.com>
2025-05-15 14:29:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
 st.title("Selamat datang di Website Informatika")
-
-st.image("IMG-20250507-WA0009.jpg")
-
+st.image("IMG-20250507-WA0009.jpg",width=200)
+st.write("\n")
+st.subheader("rasya hebat")
+st.write("RAWRR")
